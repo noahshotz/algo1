a66fb2a7c652c81a1de2f2440c05e3d1c4e0a5ca
noahshotz <max.heinrich@uusimaa.de>
2022-11-23 13:13:44 +0100

    array of linkedlist functionality implemented

--- Main.java
+++ Main.java
@@ -8,5 +8,11 @@ public class Main {
         newHashtable.put(2, "14");
         newHashtable.put(3, "15");
         newHashtable.put(4, "17");
+
+        newHashtable.put(0, "15");
+        newHashtable.put(2, "20");
+        newHashtable.put(2, "25");
+        newHashtable.put(2, "30");
+
     }
 }
--- programmierPflicht/Hashtable.java
+++ programmierPflicht/Hashtable.java
@@ -1,4 +1,5 @@
 package programmierPflicht;
+
 import java.util.LinkedList;
 
 public class Hashtable implements IntStringMap {
@@ -11,31 +12,39 @@ public class Hashtable implements IntStringMap {
     }
 
     // array of linkedlists with length k
-    @SuppressWarnings({"unchecked"})
-    LinkedList<String>[] topArr = new LinkedList[k];
+    @SuppressWarnings({ "unchecked" })
 
-    LinkedList<String> testListe = new LinkedList<String>();
+    LinkedList<String>[] testListe = new LinkedList[5];
 
     // put method
-	public String put(Integer key, String value) {
-        System.out.println("key: " + key);
-        System.out.println("value: " + value);
-
+    public String put(Integer key, String value) {
         // Key soll per Hashfunktion als Index festgelegt werden
         // Protoyp: int hashFunction(Integer key)
+        // code extern
 
-        testListe.add(key, value);
-        System.out.println("Inserted: " + testListe);
+        int innerIndex = 0;
 
+        if (testListe[key] == null) {
+            System.out.println("Key: " + key + ", add value: " + value);
+            testListe[key] = new LinkedList<>();
+            testListe[key].add(innerIndex, value);
+        } else {
+            System.out.println("linkedlist at key " + key + " already exists, insert value: " + value);
+            innerIndex++;
+            testListe[key].add(innerIndex, value);
+        }
 
-		return null;
-	}
+        System.out.println(testListe[key]);
 
-	public String get(Integer key) {
-		return null;
-	}
+        return null;
+    }
 
-	public String remove(Integer key) {
-		return null;
-	}
+    public String get(Integer key) {        
+
+        return null;
+    }
+
+    public String remove(Integer key) {
+        return null;
+    }
 }
